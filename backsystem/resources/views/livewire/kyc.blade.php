c8c47d29a87490c1a78c453e2dd0685f35c726f6
chanda chewe <chewec03@gmail.com>
2022-03-14 18:17:05 -0700

    KYC Form submission - SPA

--- backsystem/resources/views/livewire/kyc.blade.php
+++ backsystem/resources/views/livewire/kyc.blade.php
@@ -1,67 +1,99 @@
+<div>
 
 
-<!-- Modal -->
-<div wire:ignore.self class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-<div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel" style="font-weight:bold">KYC FORMS</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-       
-   
+<div>
+  <!-- Button trigger kyc Form -->
+<button class="btn btn-primary" wire:click="edit_kyc">
+  GET STARTED
+</button>
 
-   <!--End showing Success and Erros and here--> 
+</div>
+@if($kycform)
 
-   <!--Business Name-->
-     <label for="business_name">Business Name</label>
-     <input type="text" class="form-control" id="business_name" aria-describedby="business_name"  wire:model="business_name" >
-     <div>  @error('business_name') <span class="error" style="color:red">{{ $message }}</span> @enderror </div>
-   <br>
-       <!--Business Email-->    
-     <label for="business_email">Business Email</label>
-     <input type="text" class="form-control" id="business_email" aria-describedby="business_email"  wire:model="business_email" >
-     <div>  @error('business_email') <span class="error" style="color:red">{{ $message }}</span> @enderror </div>
-     <br>
-      <!--Business Phone Number-->    
-      <label for="business_email">Business Phone Number</label>
-     <input type="text" class="form-control" id="business_phone_Number" aria-describedby="business_phone_number"  wire:model="business_phone_number">
-     <div>  @error('business_phone_number') <span class="error" style="color:red">{{ $message }}</span> @enderror </div>
-  
-     <br>
-     
-      <!--Business Province-->    
-      <label for="business_province">Business Province</label>
-     <input type="text" class="form-control" id="business_email" aria-describedby="business_province"  wire:model="business_province" >
-     <div>  @error('business_province') <span class="error" style="color:red">{{ $message }}</span> @enderror </div>
-     <br>
-
-     
-      <!--Business City -->    
-      <label for="business_email">Business City</label>
-     <input type="text" class="form-control" id="business_city" aria-describedby="business_city"  wire:model="business_city" >
-     <div>  @error('business_city') <span class="error" style="color:red">{{ $message }}</span> @enderror </div>
-     <br>
-
-     
-      <!--Business Location-->    
-      <label for="business_location">Business Location</label>
-     <input type="text" class="form-control" id="business_location" aria-describedby="business_location"  wire:model="business_location" >
-     <div>  @error('business_location') <span class="error" style="color:red">{{ $message }}</span> @enderror </div>
-     <br>
-     <!--Data Ends Here-->
+<br><br>
 
 
 
 
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button wire:key type="button" class="btn btn-primary" wire:click.prevent="save()">Save changes</button>
-      </div>
-    </div>
-  </div>
+<div class="shadow p-3 mb-5 bg-white rounded w-90" id="kycform" >
+<h3 style="font-weight:bold">KYC FORM</h3>
+  <hr>
+
+<!--Success Message Here-->
+@if (session()->has('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
+
+<!--End success Message Here-->
+
+
+
+
+<!--Business Name-->
+<label for="business_name">Business Name</label>
+<input type="text" class="form-control" id="business_name" aria-describedby="business_name"  wire:model="business_name" >
+<div wire:key="foo">  @error('business_name') <span class="error" style="color:red">{{ $message }}</span> @enderror </div>
+<br>
+
+  <!--Business Email-->    
+<label for="business_email">Business Email</label>
+<input type="text" class="form-control" id="business_email" aria-describedby="business_email"  wire:model="business_email" >
+<div>  @error('business_email') <span class="error" style="color:red">{{ $message }}</span> @enderror </div>
+<br>
+ <!--Business Phone Number-->    
+ <label for="business_email">Business Phone Number</label>
+<input type="text" class="form-control" id="business_phone_Number" aria-describedby="business_phone_number"  wire:model="business_phone_number">
+<div>  @error('business_phone_number') <span class="error" style="color:red">{{ $message }}</span> @enderror </div>
+
+<br>
+
+ <!--Business Province-->    
+ <label for="business_province">Business Province</label>
+<input type="text" class="form-control" id="business_email" aria-describedby="business_province"  wire:model="business_province" >
+<div>  @error('business_province') <span class="error" style="color:red">{{ $message }}</span> @enderror </div>
+<br>
+
+
+ <!--Business City -->    
+ <label for="business_email">Business City</label>
+<input type="text" class="form-control" id="business_city" aria-describedby="business_city"  wire:model="business_city" >
+<div>  @error('business_city') <span class="error" style="color:red">{{ $message }}</span> @enderror </div>
+<br>
+
+
+ <!--Business Location-->    
+ <label for="business_location">Business Location</label>
+<input type="text" class="form-control" id="business_location" aria-describedby="business_location"  wire:model="business_location" >
+<div>  @error('business_location') <span class="error" style="color:red">{{ $message }}</span> @enderror </div>
+<br>
+
+<!--Client Id-->    
+ 
+<input type="hidden" class="form-control" wire:model="client_id" >
+<div>  @error('business_location') <span class="error" style="color:red">{{ $message }}</span> @enderror </div>
+<br>
+
+
+
+
+<!--Data Ends Here-->
+<hr>
+<button class="btn btn-danger" wire:click="cancel_kyc">CANCEL</button>
+<button class="btn btn-success" wire:click="save">SAVE</button>
+<hr>
+
+<!--Success Message Here-->
+@if (session()->has('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
+
+<!--End success Message Here-->
+
+
+</div>
+@endif
 </div>
