1746bc1528a0605f370b724f8ff48bc12ec49791
Luyando24 <cluyando2@gmail.com>
2022-03-25 15:05:03 +0800

    changed theme

--- index.php
+++ index.php
@@ -23,19 +23,596 @@
     <title>AZFOODS</title>
   </head>
   <body>
+  <style type="text/css">
+    body{
+      overflow-x: hidden;
+  margin: 0;
+  background: #fff;
+  font-family: 'Roboto', sans-serif;
+}
+.topnav{
+background: #FFF9EE;
+height: 80px;
+border-style: none;
+padding-left: 100px;
+padding-right: 100px;
+}
+@media screen and (max-width: 600px) {
+  .topnav {
+    padding: 0;
+  }
+}
 
+.logo1{
+font-weight: bolder;
+font-size: 20px;
+text-decoration: none;
+color: #616161;
+}
+.logo2{
+font-size: 20px;
+text-decoration: none;
+color: #F26505;
+}
+
+
+.addRestaurant{
+  text-decoration: none;
+  color: #535353;
+}
+.addRestaurant a: hover{
+  color: #f26505;
+}
+
+.register{
+  background: #E5E5E5;
+  border-radius: 10px;
+}
+@media screen and (max-width: 600px) {
+  .register {
+    display: none;
+  }
+}
+@media screen and (max-width: 600px) {
+  .UserProvince {
+    display: none;
+  }
+}
+.login{
+  background: #f26505;
+  border-radius: 10px;
+  color: #fff;
+}
+.register:hover{
+  opacity: 0.7;
+}
+.login:hover{
+  opacity: 0.7;
+}
+.banner{
+  display: flex;
+  justify-content: ;
+  background: #FFF9EE;
+  flex-direction: column;
+  height: 450px;
+}
+.primaryText{
+  color: #F26505;
+}
+.ProvinceBanner{
+  display: flex;
+  justify-content: ;
+  background: #CACC69;
+  height: 220px;
+  flex-direction: column;
+  text-align: center;
+
+}
+.Open{
+  background: green;
+}
+.Closed{
+  background: red;
+}
+.order{
+  margin-top: 10px;
+}
+@media screen and (max-width: 600px) {
+  .order {
+    display: flex;
+    flex-wrap: wrap;
+  }
+}
+.orderSlider{
+  width: 1000px;
+}
+
+@media screen and (max-width: 600px) {
+  .orderRow {
+    display: flex;
+    flex-wrap: wrap;
+  }
+}
+
+.orderDetails{
+  width: 100%;
+  margin-top: 2px;
+}
+@media screen and (max-width: 600px) {
+  .OrderButtons {
+    position: fixed;
+    bottom: 0;
+    display: flex;
+    justify-content: space-between;
+    background: #fff;
+    width: 100%;
+    padding: 5px;
+    overflow: hidden;
+    margin-left: -35px;
+  }
+}
+@media screen and (max-width: 600px) {
+  .orderSlider {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+  }
+}
+.orderRow{
+  display: flex;
+  justify-content: space-between;
+}
+.orderSlider img{
+  background: red;
+  height: 350px;
+}
+.OrderInfoTitle{
+  font-weight: bold;
+}
+.OrderButtons{
+  margin-top: 5px;
+  display: flex;
+  justify-content: space-between;
+}
+.btnCart{
+  background: #E5E5E5;
+  border-radius: 10px;
+}
+.btnBuy{
+  background: #f26505;
+  border-radius: 10px;
+  color: #fff;
+}
+.orderDetails{
+  padding: 30px;
+  display: flex;
+  flex-direction: column;
+  background: #fff;
+  margin-left: 2px;
+}
+.orderInfo{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.orderSlider{
+  background: #fff;
+  display: flex;
+  flex-direction: column;
+}
+.share{
+  color: green;
+}
+.foodTitle{
+  display: flex;
+  justify-content: space-between;
+  font-weight: bold;
+  margin-top: 5px;
+}
+.foodPrice{
+  font-weight: bold;
+  color: green;
+  margin-bottom: 5px;
+}
+.RestFeatures{
+  display: flex;
+  justify-content: space-between;
+  font-size: 13PX;
+
+}
+.RestFeatures .material-icons{
+
+  font-size: 16PX;
+
+}
+
+.AddressForm{
+  text-align: left;
+
+}
+ .ProvinceAdress{
+margin-top: 0px;
+}
+.AddressForm input{
+  height: 60px;
+  border-radius: 10px;
+  border: 1px solid #D4D1D1;
+}
+
+.bannerTitle{
+  margin-top: 100px;
+  font-weight: bolder;
+  font-size: 50px;
+  color: #fff;
+  font-style: italic;
+}
+@media screen and (max-width: 600px) {
+  .bannerTitle {
+    margin: 30px;
+  }
+}
+.addressInput{
+  height: 53px;
+  width: 519px;
+  border-style: none;
+  padding-left: 10px;
+}
+@media screen and (max-width: 600px) {
+  .addressInput {
+    height: 53px;
+  width: 270px;
+  border-style: none;
+  padding-left: 10px;
+  }
+}
+@media screen and (max-width: 600px) {
+  .submitAdd {
+    border-style: none;
+  height: 53px;
+  background: #0F0E0E;
+  color: #fff;
+  }
+}
+.submitAdd{
+  border-style: none;
+  height: 60px;
+  border-radius: 10px;
+  width: 150px;
+  background: #F26505;
+  color: #fff;
+}
+
+.Actions{
+  display: flex;
+  margin-top: 10px;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+.card{
+  border-radius: 10px;
+}
+.card img{
+  border-top-right-radius: 10px;
+  border-top-left-radius: 10px;
+}
+.cardActions img{
+  height: 200px;
+
+}
+.cardActions{
+  margin-top: 60px;
+}
+
+.card:hover{
+  -webkit-box-shadow: 5px 5px 14px -9px #000000; 
+box-shadow: 5px 5px 14px -9px #000000;
+    cursor: pointer;
+}
+
+.padding{
+  margin-right: 100px;
+  margin-left: 100px;
+}
+.category{
+  margin-top: 60px;
+  display: flex;
+  justify-content: space-between;
+}
+.h4{
+  font-weight: bolder;
+}
+.NearMeCard img{
+  height: 120px;
+}
+.NearMeCard{
+  margin-top: 10px;
+  width: 20rem;
+}
+.ProvinceCard{
+  margin-top: 10px;
+  width: 20rem;
+}
+@media screen and (max-width: 600px) {
+  .ProvinceCard {
+    width: 100%;
+  }
+}
+.ProvinceCard img{
+  height: 120px;
+}
+@media screen and (max-width: 600px) {
+  .NearMeCard {
+    width: 11rem;
+  }
+}
+.restCount{
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+}
+.restaurantCount .material-icons{
+  font-size: 16px;
+
+}
+.restaurantCount{
+  color: green;
+  font-size: 13px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+}
+.NearMe{
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+.Provinces{
+  background: #fff;
+  padding: 5px;
+  padding-bottom: 20px;
+  border-radius: 10px;
+  display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+}
+
+.allFood img{
+  height: 220px;
+}
+.allFood{
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+.BuyNow{
+  background: #1EC91B;
+  color: #fff;
+}
+.AddCart{
+  background: #0F0E0E;
+  color: #fff;
+}
+.groupedButtons{
+  display: flex;
+  justify-content: space-between;
+}
+
+.footer{
+  background: #616161;
+  margin-top: 10px;
+  padding: 50px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+.footerDiv a{
+  text-decoration: none;
+  color: #fff;
+}
+.footerDiv{
+  display: flex;
+  flex-direction: column;
+  line-height: 50px;
+}
+.logo11{
+  color: #fff;
+  font-weight: bolder;
+font-size: 28px;
+text-decoration: none;
+}
+
+@media screen and (max-width: 600px) {
+  .padding {
+    margin: 3px;
+  }
+}
+.loginPane{
+  text-align: center;
+  margin-top: 90px;
+
+}
+
+.loginInput{
+  border-style: none;
+  border-radius: 10px;
+  height: 40px;
+  padding: 10px;
+  background: #E5E5E5;
+  width: 100%;
+}
+.loginSubmit{
+  border-style: none;
+  border-radius: 10px;
+  height: 40px;
+  padding: 10px;
+  width: 45%;
+  color: #fff;
+  background: #1EC91B;
+}
+.btn a{
+  text-decoration: none;
+  color: #fff;
+}
+.btn-register a{
+  text-decoration: none;
+  color: #000;
+}
+
+@media screen and (max-width: 600px) {
+  .loginPicture {
+    display: none;
+  }
+}
+@media screen and (max-width: 600px) {
+  .loginPane {
+    margin-top: 20px;
+  }
+}
+.DeliveryArea{
+  margin-top: 20px;
+}
+.brand{
+  display: flex;
+  flex-direction: column;
+}
+.cartMessage{
+  display: flex;
+  justify-content: center;
+  background: #CACC69;
+  font-size: 25px;
+  font-weight: bolder;
+  color: #fff;
+  width: 800px;
+  padding: 50px;
+  height: 400px;
+}
+.cartHolder{
+  display: flex;
+  margin-top: 20px;
+}
+@media screen and (max-width: 600px) {
+  .cartMessage {
+    display: none;
+  }
+}
+.cartItemCol{
+  background: #fff;
+  display: flex;
+  height: 105px;
+  margin-left: 5px;
+  border-radius: 10px;
+  width: 100%;
+}
+.cartItemsRow{
+  background: #E5E5E5;
+}
+
+.cartPicture img{
+  width: 150px;
+  border-radius: 10px;
+}
+.cartItemBody{
+  display: flex;
+  flex-direction: column;
+  padding: 5px;
+}
+.cartItemHeader{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+}
+.CartFoodName{
+  font-weight: bolder;
+}
+.cartPicture img{
+  width: 160px;
+  height: 100px;
+}
+@media screen and (max-width: 600px) {
+  .cartPicture img {
+    width: 70px;
+  height: 50px;
+  }
+}
+.cartPicture{
+  background: red;
+}
+.cardItems{
+  display: flex;
+  justify-content: space-between;
+}
+.cartAddress{
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+}
+.cardItems img{
+  width: 30px;
+  border-radius: 0;
+}
+.paymentMethods{
+  display: flex;
+  justify-content: center;
+}
+.Primary{
+  background: #1EC91B;
+  color: #fff;
+}
+.PaymentButtons{
+  display: flex;
+  justify-content: center;
+}
+.HeroMessage{
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+}
+.bigHero{
+ font-size: 36px;
+font-weight: bolder;
+text-align: left;
+color: #616161;
+}
+.smallHero{
+  text-align: left;
+  font-size: 20px;
+  color: #303030;
+  }
+.userLocation{
+  color: #F26505;
+  margin: 5px;
+}
+.HeroImage{
+  text-align: center;
+  background: #FCDD3E;
+  padding: 10px;
+}
+.cart{
+  color: #F26505;
+  font-size: 25px;
+}
+.searchContainer{
+  background: #fff;
+  padding: 5px;
+  padding-bottom: 20px;
+  width: 905px;
+  padding-left: 10px;
+  }
+  </style>
   	<!--Top navigation bar-->
-    <nav class="navbar topnav sticky-top">
+    <nav class="navbar topnav">
     	<div class="container-fluid">
         
-    		<a class="navbar-brand" href="#"><span class="logo1">AZ</span><span class="logo2">FOODS</span></a>
-        <i class="UserProvince">Delivering to: <span class="city" style="font-weight: bolder;">Lusaka</span></i>
+    		<a class="navbar-brand" href="#"><img src="images/logo.png" height="50" width="50"><span class="logo2">azfoods</span></a>
+        <div class="UserProvince">Delivering to: <span class="city" style="color: #616161;">Lusaka</span></div>
         <div>
     		<div class="AddRes">
     	    </div>
     		<div class="loginRegister">
-    		<button class="btn btn btn-register register"><a href="backsystem/public/register">Register</a></button>
-    		<button class="btn btn login"><a href="backsystem/public/login">Login</a></button>
+    		<button class="btn btn login"><a href="backsystem/public/login">Sign in</a></button>
         <button class="btn btn-lg cart"><span class="material-icons">shopping_cart</span></button>
 
     	</div>
@@ -44,13 +621,33 @@
 
     <!--Banner-->
     <div class="banner">
-    	<div class="bannerTitle">
-    		Are You Hungry?
+    	<div class="row Hero mt-5">
+    		<div class="col HeroMessage padding" style="background-color:">
+         <div class="bigHero"> We deliver your deliver your <span class="primaryText"> favorite </span> food in <span class="primaryText"> minutes </span></div>
+         <div class="smallHero mt-2">Buy from nearest restaurants
+
+
+
+<br>and get your food in<br>
+<span class="primaryText"> minutes </span></div>
+        </div>
+
+        <div class="col HeroImage padding" style="background-color:">
+         <img src="images/here.png">
+        </div>
+        <div class="row searching padding" style="background-color:">
+          <div class="searchContainer">
+         <form class="AddressForm">
+        <input type="text" name="address" class="addressInput" placeholder="Enter your delivery address">
+        <badge class="userLocation"><span class="material-icons">my_location</span></badge>
+        <button class="btn submitAdd">Delivery</button>
+        or
+        <button class="btn submitAdd">Pick-Up</button>
+      </form>
+    </div>
+        </div>
     	</div>
-    	<form class="AddressForm">
-    		<input type="text" name="address" class="addressInput" placeholder="Search Food/restaurant">
-        <button type="submit" class="btn submitAdd"><span class="material-icons">search</span></button>
-    	</form>
+    	 
     </div>
 
     
@@ -59,232 +656,10 @@
     <div class="category padding">
       <h4>Select province</h4>
     </div>
-    <section class="Provinces padding">
-      <div class="card ProvinceCard">
-  <img src="images/lusaka.jpg" class="card-img-top" alt="...">
-  <div class="card-body restCount">
-    <h5 class="card-title">Lusaka</h5>
-    <div class="restaurantCount">
-      <span class="material-icons">storefront</span>
-      <div class="rCount">
-        : 500
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="card ProvinceCard">
-  <img src="images/ndola.jpg" class="card-img-top" alt="...">
-  <div class="card-body restCount">
-    <h5 class="card-title">Copperbelt</h5>
-    <div class="restaurantCount">
-      <span class="material-icons">storefront</span>
-      <div class="rCount">
-        : 500
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="card ProvinceCard">
-  <img src="images/liv.jpg" class="card-img-top" alt="...">
-  <div class="card-body restCount">
-    <h5 class="card-title">Southern</h5>
-    <div class="restaurantCount">
-      <span class="material-icons">storefront</span>
-      <div class="rCount">
-        : 500
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="card ProvinceCard">
-  <img src="images/eastern.jpg" class="card-img-top" alt="...">
-  <div class="card-body restCount">
-    <h5 class="card-title">Eastern</h5>
-    <div class="restaurantCount">
-      <span class="material-icons">storefront</span>
-      <div class="rCount">
-        : 500
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="card ProvinceCard">
-  <img src="images/western.jpg" class="card-img-top" alt="...">
-  <div class="card-body restCount">
-    <h5 class="card-title">Western</h5>
-    <div class="restaurantCount">
-      <span class="material-icons">storefront</span>
-      <div class="rCount">
-        : 500
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="card ProvinceCard">
-  <img src="images/north-west.jpg" class="card-img-top" alt="...">
-  <div class="card-body restCount">
-    <h5 class="card-title">North-western</h5>
-    <div class="restaurantCount">
-      <span class="material-icons">storefront</span>
-      <div class="rCount">
-        : 500
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="card ProvinceCard">
-  <img src="images/location.jpg" class="card-img-top" alt="...">
-  <div class="card-body restCount">
-    <h5 class="card-title">Province Name</h5>
-    <div class="restaurantCount">
-      <span class="material-icons">storefront</span>
-      <div class="rCount">
-        : 500
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="card ProvinceCard">
-  <img src="images/location.jpg" class="card-img-top" alt="...">
-  <div class="card-body restCount">
-    <h5 class="card-title">Province Name</h5>
-    <div class="restaurantCount">
-      <span class="material-icons">storefront</span>
-      <div class="rCount">
-        : 500
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="card ProvinceCard">
-  <img src="images/location.jpg" class="card-img-top" alt="...">
-  <div class="card-body restCount">
-    <h5 class="card-title">Province Name</h5>
-    <div class="restaurantCount">
-      <span class="material-icons">storefront</span>
-      <div class="rCount">
-        : 500
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="card ProvinceCard">
-  <img src="images/location.jpg" class="card-img-top" alt="...">
-  <div class="card-body restCount">
-    <h5 class="card-title">Province Name</h5>
-    <div class="restaurantCount">
-      <span class="material-icons">storefront</span>
-      <div class="rCount">
-        : 500
-      </div>
-    </div>
-  </div>
-</div>
-    </section>
+    
 
     
-    <!--All foods-->
-    <div class="category padding">
-    	<h4>Recommended for you</h4>
-    	<h4>See All</h4>
-    </div>
-
-    <section class="allFood padding">
-    	<div class="card NearMeCard" style="width: 29rem;">
-  <img src="images/ns.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Food Name</h5>
-    <p class="card-text">Food Description</p>
     
-    
-  </div>
-</div>
-
-<div class="card NearMeCard" style="width: 29rem;">
-  <img src="images/b.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Food Name</h5>
-    <p class="card-text">Food Description</p>
-    
-    
-  </div>
-</div>
-
-<div class="card NearMeCard" style="width: 29rem;">
-  <img src="images/pz.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Food Name</h5>
-    <p class="card-text">Food Description</p>
-   
-    
-  </div>
-</div>
-
-<div class="card NearMeCard" style="width: 29rem;">
-  <img src="images/f.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Food Name</h5>
-    <p class="card-text">Food Description</p>
-    
-    
-  </div>
-</div>
-
-<div class="card NearMeCard" style="width: 29rem;">
-  <img src="images/ck.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Food Name</h5>
-    <p class="card-text">Food Description</p>
-   
-    
-  </div>
-</div>
-
-<div class="card NearMeCard" style="width: 29rem;">
-  <img src="images/hn.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Food Name</h5>
-    <p class="card-text">Food Description</p>
-    
-    
-  </div>
-</div>
-    </section>
-   <!--Actions-->
-    <section class="Actions padding">
-      <div class="card cardActions" style="width: 28rem;">
-  <img src="images/ck.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Browse Foods</h5>
-    <p class="card-text">Find your favorite foods</p>
-  </div>
-</div>
-
-<div class="card cardActions" style="width: 28rem;">
-  <img src="images/res.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Add restaurant</h5>
-    <p class="card-text">Add your restaurant and start selling</p>
-  </div>
-</div>
-
-<div class="card cardActions" style="width: 28rem;">
-  <img src="images/deliver.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Deliver Food</h5>
-    <p class="card-text">Register to deliver and earn money</p>
-  </div>
-</div>
-    </section>
     <!--footer-->
     <div class="footer">
     	<div>
